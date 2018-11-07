8799211ba419dc923702feeecfa2b2106d619493
kafka <a50541853@gmail.com>
2018-11-07 17:34:26 +0800

    顶栏去除完成

---
+++ component/divide_item/divide_item.js
@@ -0,0 +1,23 @@
+// component/divide_item/divide_item.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    divide_item:"222222",
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
